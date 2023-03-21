819cbd9e56e97ac040a2fa5b87d694ea135b5dd7
Thuku Elvis <elvisndichu2@gmail.com>
2023-03-20 18:19:28 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet in lowercase,and then in uppercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	putcher(letter);
+	putchar('\n');
+	return (0);
+}
