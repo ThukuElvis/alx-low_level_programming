04b7c69310f4f4020ecebb3e2afd7969a697127c
Thuku Elvis <elvisndichu2@gmail.com>
2023-03-22 14:18:57 -0700

    0x02-functions_nested_loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet- funtion that print alphabet in lower case
+ * Return : always zero
+ */
+
+void print_alphabet(void);
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z'; i++)
+		_putchar(i);
+	_putcher('\n');
+
+	return (0);
+}
