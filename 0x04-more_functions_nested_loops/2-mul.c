9bfe5fb631b63a42f239e02d38e1a227048e27da
Thuku Elvis <elvisndichu2@gmail.com>
2023-03-24 15:38:21 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: - The first integer
+ * @b: The second integer
+ * Return: Always
+ */
+
+void print_numbers(void)
+{
+int result = a * b;
+
+return (result);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: - The first integer
+ * @b: The second integer
+ * Return: Always
+ */
+
+void print_numbers(void)
+{
+	int result = a * b;
+	return (result);
+}
