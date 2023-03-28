53b17c0e4e4d6c52102f2916b080c3a326869acb
Thuku Elvis <elvisndichu2@gmail.com>
2023-03-28 13:06:44 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - checks the lenght of the string
+ * @s:-string to be checked
+ * Return:The lenght of the the string
+ */
+
+int _strlen(char *s)
+{
+int string_lenght = 0;
+while (s[string_lenght])
+	string_length++;
+return (string_length);
+
+}
